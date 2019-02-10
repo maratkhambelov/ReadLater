546a3d4964bc37ff16ddee7364e7264bbf8bdb2c
student <student@localhost>
2019-02-10 20:41:06 +0300

    async/await

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,55 +1,58 @@
-import { Http } from './http.js';
+import {Http} from './http.js';
 
 const http = new Http('http://localhost:7777/items');
 const ulEl = document.querySelector('#tasks');
 let cachedItems = [];
 
-ulEl.addEventListener('click', (evt) => {
-   // evt.target -> EventTarget -> Object
-   if (evt.target.getAttribute('data-action') === 'remove') {
-      // Для упрощения -> while
-      const id = evt.target.parentElement.getAttribute('data-id');
-      http.removeById(id, () => {
-         loadData();
-      });
-   }
+ulEl.addEventListener('click', async (evt) => {
+    // evt.target -> EventTarget -> Object
+    if (evt.target.getAttribute('data-action') === 'remove') {
+        // Для упрощения -> while
+        const id = evt.target.parentElement.getAttribute('data-id');
+        await http.removeById(id);
+        await loadData();
+    }
 });
 
-ulEl.addEventListener('change', (evt) => {
-   if (evt.target.getAttribute('data-change') === 'done') {
-      // console.log(evt.target.checked);
-      // memory -> server
-      // closest('li');
-      const id = Number(evt.target.parentElement.parentElement.getAttribute('data-id'));
-      const item = cachedItems.find((value) => {
-         return value.id === id;
-      });
-      item.done = evt.target.checked;
-      http.save(item, () => {
-         loadData();
-      });
-   }
+ulEl.addEventListener('change', async (evt) => {
+    if (evt.target.getAttribute('data-change') === 'done') {
+        const id = Number(evt.target.parentElement.parentElement.getAttribute('data-id'));
+        const item = cachedItems.find((value) => {
+            return value.id === id;
+        });
+        item.done = evt.target.checked;
+        await http.save(item);
+        await loadData();
+    }
 });
 
-loadData();
+loadData(); // -> Promise
 
-function loadData() {
-   http.getAll((items) => {
-      cachedItems = items;
-      ulEl.innerHTML = '';
+async function loadData() {
+    try {
+        const response = await http.getAll(); // http.getAll() -> Promise (запоминает эту точку, чтобы вернуться сюда тогда, когда промис разрешится)
+        cachedItems = await response.json(); // Promise ->
+        console.log(cachedItems);
 
-      items.forEach((item) => {
-         const liEl = document.createElement('li');
-         // liEl.item = item;
-         liEl.setAttribute('data-id', item.id);
-         liEl.innerHTML = `
-            <label><input data-change="done" type="checkbox"> ${item.name}</label>
-            <button data-action="remove">Remove</button>
-         `;
-         const checkboxEl = liEl.querySelector('[data-change=done]');
-         checkboxEl.checked = item.done;
-         ulEl.appendChild(liEl);
-      });
-   });
+        ulEl.innerHTML = '';
 
+        cachedItems.forEach((item) => {
+            const liEl = document.createElement('li');
+            // liEl.item = item;
+            liEl.setAttribute('data-id', item.id);
+            liEl.innerHTML = `
+                 <label><input data-change="done" type="checkbox"> ${item.name}</label>
+                 <button data-action="remove">Remove</button>
+              `;
+            const checkboxEl = liEl.querySelector('[data-change=done]');
+            checkboxEl.checked = item.done;
+            ulEl.appendChild(liEl);
+        });
+
+    } catch (e) {
+        // e -> ошибка
+        console.log(e);
+    } finally {
+        console.log('we finished');
+    }
 }
--- frontend/src/js/http.js
+++ frontend/src/js/http.js
@@ -3,47 +3,23 @@ export class Http {
         this.url = url;
     }
 
-    getAll(callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', this.url);
-        xhr.addEventListener('load', () => {
-            // когда был отправлен запрос и получен ответ
-            // 404, 405
-            console.log('loaded');
-            if (xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                callback(data);
-            }
-        });
-        xhr.addEventListener('error', () => {
-           // когда ответ не получен, не удалось отправить запрос и т.д.
-        });
-        xhr.addEventListener('loadend', () => {
-           console.log('request completed');
-        });
-        xhr.send();
+    getAll() {
+        return fetch(this.url);
     }
 
-    save(item, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', this.url);
-        xhr.addEventListener('load', () => {
-            if (xhr.status === 200) {
-                callback();
-            }
+    save(item) {
+        return fetch(this.url, {
+           body: JSON.stringify(item),
+           method: 'POST',
+           headers: {
+               'Content-Type': 'application/json'
+           }
         });
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify(item));
     }
 
-    removeById(id, callback) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('DELETE', `${this.url}/${id}`);
-        xhr.addEventListener('load', () => {
-            if (xhr.status === 200) {
-                callback();
-            }
+    removeById(id) {
+        return fetch(`${this.url}/${id}`, {
+            method: 'DELETE'
         });
-        xhr.send();
     }
 }
